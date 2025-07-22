6f69444fc944201fb930ced16b4fe7f4257af702
ASC <ASC@Rahul>
2025-07-22 12:37:58 +0530

    add post

--- src/main/java/com/booking/repository/CourseRepo.java
+++ src/main/java/com/booking/repository/CourseRepo.java
@@ -11,36 +11,35 @@ import com.booking.entity.Registration;
 import com.booking.utility.CourseUtility;
 
 public class CourseRepo {
-	
-	Scanner sc = new Scanner(System.in);
 
+	Scanner sc = new Scanner(System.in);
+    // add couse
 	public void addCourse() {
 		Registration registration = new Registration();
-		
+
 		System.out.println("Enter your student name");
-		String name= sc.next();
+		String name = sc.next();
 		registration.setStudentName(name);
 
 		System.out.println("Enter your course name");
 		String course = sc.next();
 		registration.setCourseName(course);
-		
+
 		registration.setRegistrationDate(new java.sql.Date(System.currentTimeMillis()));
-		
+
 		System.out.println("Enter Fees paid ammount");
-		double fee =sc.nextDouble();
+		double fee = sc.nextDouble();
 		registration.setFeesPaid(fee);
-		
+
 		SessionFactory factory = CourseUtility.getfactory();
 		Session session = factory.openSession();
 		Transaction transaction = session.beginTransaction();
-		
+
 		session.persist(registration);
-		
+
 		transaction.commit();
 		session.close();
-		
-		
+
 	}
 
 }
