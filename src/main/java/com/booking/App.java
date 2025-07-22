fd11905d4c552647b83c04a005399fe2b7aa9f80
ASC <ASC@Rahul>
2025-07-22 11:47:01 +0530

    create database table using xml file

---
+++ src/main/java/com/booking/App.java
@@ -0,0 +1,15 @@
+package com.booking;
+
+import com.booking.utility.CourseUtility;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+       CourseUtility.getfactory();
+    }
+}
---
+++ src/main/java/com/booking/entity/Students.java
@@ -0,0 +1,74 @@
+package com.booking.entity;
+
+import java.sql.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Registration")
+public class Students {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String studentName;
+	
+	private String courseName;
+	
+	private Date registrationDate;
+	
+	private double feesPaid;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getStudentName() {
+		return studentName;
+	}
+
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+
+	public String getCourseName() {
+		return courseName;
+	}
+
+	public void setCourseName(String courseName) {
+		this.courseName = courseName;
+	}
+
+	public Date getRegistrationDate() {
+		return registrationDate;
+	}
+
+	public void setRegistrationDate(Date registrationDate) {
+		this.registrationDate = registrationDate;
+	}
+
+	public double getFeesPaid() {
+		return feesPaid;
+	}
+
+	public void setFeesPaid(double feesPaid) {
+		this.feesPaid = feesPaid;
+	}
+
+	@Override
+	public String toString() {
+		return "Students [id=" + id + ", studentName=" + studentName + ", courseName=" + courseName
+				+ ", registrationDate=" + registrationDate + ", feesPaid=" + feesPaid + "]";
+	}
+	
+
+}
